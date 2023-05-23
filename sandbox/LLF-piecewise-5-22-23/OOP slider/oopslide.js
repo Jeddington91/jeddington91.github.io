ba947ee5f8ac598be09c02663b34e94268df2f46
James Eddington <Jeddington91@users.noreply.github.com>
2023-05-23 12:08:20 -0600

    updates

--- sandbox/LLF-piecewise-5-22-23/OOP slider/oopslide.js
+++ sandbox/LLF-piecewise-5-22-23/OOP slider/oopslide.js
@@ -51,7 +51,7 @@ class Slider {
 		this.autoScrollTimeout = setTimeout(() => {
 			this.sectionIndex = (this.sectionIndex + 1) % this.numberOfSlides;
 			if (this.sectionIndex >= this.numberOfSlides / this.visibleSlides) { //Check for multislider
-  			this.sectionIndex = 0; // Set sectionIndex to the first slide
+				this.sectionIndex = 0; // Set sectionIndex to the first slide
 			}
 			this.slide(this.sectionIndex);
 			this.autoScroll();
@@ -78,21 +78,88 @@ class Slider {
 
 class MultiSlider {
   constructor(sliderSelector, controlsSelector, visibleSlides, slideMargin) {
-    // Initialize the slider and controls elements based on the provided selectors
     this.sliderElement = document.querySelector(sliderSelector);
     this.controlsElement = document.querySelector(controlsSelector);
     this.sectionIndex = 0;
     this.visibleSlides = visibleSlides;
     this.slideMargin = slideMargin;
-    
-    // get the number of slides from the current slider
+
+    this.slideWidth = ((100 - (this.slideMargin * (this.visibleSlides - 1))) / this.visibleSlides).toFixed(2);
     this.numberOfSlides = this.sliderElement.getElementsByTagName('section').length;
-    // dynamically set the width of the current slider
-    this.sliderElement.style.width = `${(this.numberOfSlides * (this.slideWidth + this.slideMargin))}%`;
-    // Slide to the current section
-    this.sliderElement.style.transform = `translate(${this.sectionIndex * -(this.slideWidth + this.slideMargin)}%)`;
+    this.sliderElement.style.width = `${(this.numberOfSlides * (this.slideWidth + this.slideMargin)) - this.slideMargin}%`;
+    this.sliderElement.style.transform = `translate(${this.sectionIndex * -(this.slideWidth + this.slideMargin) * this.visibleSlides}%)`;
+
+    this.autoScrollTimeout = null; // Added to keep track of the auto-scroll timeout
+  }
+	// Multiple slides requires margin in between (or 0 last parameter if not)
+	applySlideMargin() {
+    const slides = this.sliderElement.querySelectorAll('section');
+		slides.forEach(slide => {
+			slide.style.marginRight = `${this.slideMargin}%`;
+		});
+	}
+	
+	slide(index) {
+    // Remove 'selected' class from previously selected indicator
+		const previousSelected = this.controlsElement.querySelector('.selected');
+			if (previousSelected) previousSelected.classList.remove('selected');
+		// Add 'selected' class to the current indicator
+		const currentIndicator = this.controlsElement.children[index];
+			if (currentIndicator) {
+				currentIndicator.classList.add('selected');
+			}
+
+		// Slide to the current section
+		this.sliderElement.style.transform = `translate(${index * -100}%)`;
+		// Update the active indicator
+		this.updateActiveIndicator();
   }
 
+  updateActiveIndicator() {
+    const indicators = this.controlsElement.querySelectorAll('li');
+
+    // Remove the .selected class from all indicators
+    indicators.forEach(indicator => {
+      indicator.classList.remove('selected');
+    });
+
+    // Add the .selected class to the active indicator
+    const activeIndicator = indicators[this.sectionIndex];
+    if (activeIndicator) {
+      activeIndicator.classList.add('selected');
+    }
+  }
+	// Auto-scroll method (7s)
+	autoScroll() {
+		clearTimeout(this.autoScrollTimeout);
+		this.autoScrollTimeout = setTimeout(() => {
+			this.sectionIndex = (this.sectionIndex + 1) % this.numberOfSlides;
+			if (this.sectionIndex >= this.numberOfSlides / this.visibleSlides) { //Check for multislider
+				this.sectionIndex = 0; // Set sectionIndex to the first slide
+			}
+			this.slide(this.sectionIndex);
+			this.autoScroll();
+		}, 7000);
+	}
+	//.li indicator listener initialization method
+    setupListeners() {
+      // setup listeners for indicators and arrows
+		this.controlsElement.querySelectorAll('li').forEach((indicator, ind) => {
+			indicator.addEventListener('click', () => {
+				this.sectionIndex = ind;
+				this.slide(this.sectionIndex);
+				this.autoScroll();
+			});
+		});
+    }
+	
+	//Method to initialize controls and autoscrolling
+    init() {
+	   this.applySlideMargin();
+       this.setupListeners();
+       this.autoScroll();
+     }
+}
    // Create & Initialize new slider's here. (.slider_container.(*slider name by class (div parent of "slider")*) .slider, .slider_container.* .controls );
    // Place each used on each page in script tag of each page.
 
